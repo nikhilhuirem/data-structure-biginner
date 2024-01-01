b286e3e5e437103fabf1d39a4e4a13e6c7d81444
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-01 23:51:38 +0530

    added linkedlist

---
+++ LinledList/singlyLL.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value =  value
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def printLL(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.next = None
+        self.length += 1
+        return True
+
+    def pop(self) -> int:
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
