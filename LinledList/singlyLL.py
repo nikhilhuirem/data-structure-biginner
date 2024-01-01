2831612b49fe5dc9ef16d6e2856b34fec328e483
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-02 00:08:02 +0530

    added set and get

--- LinledList/singlyLL.py
+++ LinledList/singlyLL.py
@@ -43,3 +43,39 @@ class SinglyLinkedList:
             self.tail = None
         return temp.value
 
+    def Prepend(self, value) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next =  None
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
