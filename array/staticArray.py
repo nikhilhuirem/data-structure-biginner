b6aeafc27f9836a0832dabba633c2b3f5aa85e82
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-01 13:22:10 +0530

    added insertMiddle

--- array/staticArray.py
+++ array/staticArray.py
@@ -7,3 +7,8 @@ def removeEnd(arr, length):
     if length > 0:
         arr[length - 1] = 0
 
+def insertMiddle(arr, n, i, length):
+    for index in range(length -1, i - 1, -1):
+        arr[index + 1] = arr[index]
+
+    arr[i] = n
