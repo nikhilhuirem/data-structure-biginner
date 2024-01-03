288894b8d56e13c42feee437960553224bb78810
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-03 22:26:56 +0530

    Added arrays

---
+++ array/arrays.py
@@ -0,0 +1,11 @@
+gas_prices = []
+
+gas_prices.append(346)
+gas_prices.append(360)
+gas_prices.append(354)
+
+print(gas_prices)
+gas_prices.pop()
+print(gas_prices)
+gas_prices.insert(1,3)
+print(gas_prices)
---
+++ array/mergingMeetingTimes.py
@@ -0,0 +1,5 @@
+'''
+    Problem Statement: Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.
+
+    To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as a tuple of integers (start_time, end_time). These integers represent the number of 30-minute blocks past 9:00am.
+'''
