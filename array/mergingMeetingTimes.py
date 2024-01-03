e96ab9bcb9cf329d52048d4c01f70cc62af975d7
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-03 22:54:33 +0530

    solved merging meeting times

--- array/mergingMeetingTimes.py
+++ array/mergingMeetingTimes.py
@@ -3,3 +3,17 @@
 
     To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as a tuple of integers (start_time, end_time). These integers represent the number of 30-minute blocks past 9:00am.
 '''
+
+def mergingMeeting(arr):
+    arr.sort()
+    merged = [arr[0]]
+    for current_start, current_end in arr[1:]:
+        last_start, last_end = merged[-1]
+
+        if current_start <= last_end:
+            merged[-1] = (last_start, max(last_end, current_end))
+        else:
+            merged.append((current_start, current_end))
+    return merged
+
+print(mergingMeeting([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]))
