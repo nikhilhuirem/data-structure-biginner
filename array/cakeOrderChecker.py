cc76e83ec60239811bf3130c972e52496aadd863
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-04 23:01:29 +0530

    added Cake order problem

---
+++ array/cakeOrderChecker.py
@@ -0,0 +1,13 @@
+'''
+    Problem Statement: My cake shop is so popular, I'm adding some tables and hiring wait staff so folks can have a cute sit-down cake-eating experience.
+
+    I have two registers: one for take-out orders, and the other for the other folks eating inside the cafe. All the customer orders get combined into one list for the kitchen, where they should be handled first-come, first-served.
+
+    Recently, some customers have been complaining that people who placed orders after them are getting their food first. Yikes—that's not good for business!
+
+    To investigate their claims, one afternoon I sat behind the registers with my laptop and recorded:
+
+    The take-out orders as they were entered into the system and given to the kitchen. (take_out_orders)
+    The dine-in orders as they were entered into the system and given to the kitchen. (dine_in_orders)
+    Each customer order (from either register) as it was finished by the kitchen. (served_orders)
+'''
