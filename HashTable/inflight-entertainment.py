178082cc3bae1cf69b6a5c6123554a3c7539346f
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-10 19:09:12 +0530

    added inflight problem

--- HashTable/inflight-entertainment.py
+++ HashTable/inflight-entertainment.py
@@ -2,3 +2,19 @@
     You've built an inflight entertainment system with on-demand movie streaming.
 
 '''
+
+def flightSystem(fL, mL)-> bool:
+    if fL < 2 or len(mL) < 2:
+        return False
+    
+    movie_length = set()
+    for movie in mL:
+        movieMin = fL - movie
+        
+        if movieMin in movie_length:
+            return True
+        movie_length.add(movie)
+    return False
+
+print(flightSystem(5, [2, 3, 4, 5]))
+ 
