eb68a7b55679fe4f823ec9bbcc910838cd630ac7
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-10 20:49:22 +0530

    added solution

--- HashTable/permutationPalindrome.py
+++ HashTable/permutationPalindrome.py
@@ -3,4 +3,14 @@
 '''
 
 def has_palindrome_permutation(the_string):
-    
+    unpaired_characters = set()
+
+    for char in the_string:
+        if char in unpaired_characters:
+            unpaired_characters.remove(char)
+        else:
+            unpaired_characters.add(char)
+    return len(unpaired_characters) <= 1
+
+ans = has_palindrome_permutation("livci")
+print(ans)
