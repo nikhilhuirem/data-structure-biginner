1182bea513f639e691557f96fc546e30167200a1
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-13 21:23:26 +0530

    added pop-first function

--- Doubly_linked_list/dll.py
+++ Doubly_linked_list/dll.py
@@ -55,3 +55,16 @@ class CircularLL:
         self.lenght += 1
         return True
     
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None      
+        self.length -= 1
+        return temp
