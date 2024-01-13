8438b4e4b2a7672966820517d2d6d93cf0302885
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-13 20:51:36 +0530

    added print function in dll

--- Doubly_linked_list/dll.py
+++ Doubly_linked_list/dll.py
@@ -10,3 +10,10 @@ class CircularLL:
         self.head = new_node
         self.tail = new_node
         length = 1
+
+    def print(self):
+        temp = self.head
+        for _ in range(self.length):
+            print(temp.value)
+            temp = temp.next
+    
