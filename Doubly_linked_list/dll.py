2864fb0b3be78f9506e9f37a632441f7af87a522
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-13 21:24:29 +0530

    added set  function

--- Doubly_linked_list/dll.py
+++ Doubly_linked_list/dll.py
@@ -81,3 +81,10 @@ class CircularLL:
             for _ in range(self.length - 1, index, -1):
                 temp = temp.prev  
         return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
