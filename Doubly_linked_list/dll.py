4504f758bc124e9b0034005142474f9defa6c9bf
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-13 21:24:02 +0530

    added get function

--- Doubly_linked_list/dll.py
+++ Doubly_linked_list/dll.py
@@ -68,3 +68,16 @@ class CircularLL:
             temp.next = None      
         self.length -= 1
         return temp
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev  
+        return temp
