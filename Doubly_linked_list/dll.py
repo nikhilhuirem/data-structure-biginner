16e795c31f42c513c7f92617b3c16d4c5401fb6a
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-13 20:59:12 +0530

    added append in dll

--- Doubly_linked_list/dll.py
+++ Doubly_linked_list/dll.py
@@ -17,3 +17,14 @@ class CircularLL:
             print(temp.value)
             temp = temp.next
     
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
