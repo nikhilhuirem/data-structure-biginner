8207eff5ee980132820ebdf15a1b04909567e986
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-14 19:39:43 +0530

    added push in stack

--- stacks/intro.py
+++ stacks/intro.py
@@ -30,4 +30,12 @@ class Stack:
         self.top = new_node
         self.height = 1
     
-    def 
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+    
