1e7f899ca45740e41d26915cc6a20d3b8eb520cc
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-15 19:33:35 +0530

    added insert in tree bst

--- Trees/bst.py
+++ Trees/bst.py
@@ -10,4 +10,19 @@ class Binary_Search_Tree:
     
     def insert(self, value):
         new_node = Node(value)
-        
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        if new_node.value < temp.value:
+            if temp.left is None:
+                temp.left = new_node
+                return True
+            temp = self.left
+        else:
+            if temp.right is None:
+                temp.right = new_node
+                return True
+            temp = self.right
+
+            
