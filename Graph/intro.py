de051358c2e69bd14e8048ca77cb7cf1ad468343
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-21 11:59:16 +0530

    added add vertex/node in graph

--- Graph/intro.py
+++ Graph/intro.py
@@ -4,5 +4,14 @@ class Graph:
     
     def printGraph(self):
         for vertex in self.adj_list:
-            print("Vertex", ":", self.adj_list[vertex])
+            print(vertex, ":", self.adj_list[vertex])
     
+    def addVertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+g1  = Graph()
+g1.addVertex("A")
+g1.printGraph()
