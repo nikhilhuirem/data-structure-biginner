3a7bf90f1765b3ae4d96d7da9864a12013e53630
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-21 12:03:55 +0530

    added addEdges in graph

--- Graph/intro.py
+++ Graph/intro.py
@@ -11,7 +11,17 @@ class Graph:
             self.adj_list[vertex] = []
             return True
         return False
+    def addEdges(self, v1, v2)-> bool:
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
     
+
 g1  = Graph()
 g1.addVertex("A")
+g1.addVertex("B")
+g1.printGraph()
+g1.addEdges("A", "B")
 g1.printGraph()
