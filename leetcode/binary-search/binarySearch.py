b8a6c86450850be49f318f0a35a6bfe769fd05e2
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-27 11:04:44 +0530

    added leetcode coding question

---
+++ leetcode/binary-search/binarySearch.py
@@ -0,0 +1,13 @@
+class BinarySearch:
+    def search(nums: List[int], target: int) -> int:
+        l , r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l)//2)
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
