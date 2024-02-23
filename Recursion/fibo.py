2cb2b041fb7b3bc7432da03c2b04fcb88bbe4327
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-02-23 22:34:19 +0530

    changes

--- Recursion/fibo.py
+++ Recursion/fibo.py
@@ -5,4 +5,18 @@ def fibonacci(n):
         return n
 
     # Recursive case: fib(n) = fib(n - 1) + fib(n - 1)
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2) 
+
+
+def func3():
+    print("Three")
+
+def func2():
+    func3()
+    print("Two")
+
+def func1():
+    func2()
+    print("One")
+
+func1()
