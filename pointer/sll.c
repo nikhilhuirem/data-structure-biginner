f4601a1d961967ca7431bd2e041a0c947467334b
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-03-11 20:23:20 +0530

    added print for sll

--- pointer/sll.c
+++ pointer/sll.c
@@ -7,6 +7,16 @@ struct node
     struct node *link;
 };
 
+
+void print_list(struct node *head) {
+    struct node *p;
+    p = head->link;
+    while(p != NULL) {
+        printf("%c", p->c);
+        p = p->link;
+    }
+}
+
 int main(void) 
 {
     struct node *head;
