249b89d0dfe2d7d999a5fc1ffa2b5119c4cc582d
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-03-15 11:10:01 +0530

    added sll

--- pointer/sll.c
+++ pointer/sll.c
@@ -10,7 +10,7 @@ struct node
 
 void print_list(struct node *head) {
     struct node *p;
-    p = head->link;
+    p = head;
     while(p != NULL) {
         printf("%c", p->c);
         p = p->link;
@@ -36,7 +36,30 @@ void insertAtEnd(struct node *head) {
 
 int main(void) 
 {
+    //Initialize nodes
     struct node *head;
-    struct node *p;
-    head = (struct node *)malloc(sizeof(struct node)); 
+    struct node *one = NULL;
+    struct node *two = NULL;
+    struct node *three = NULL;
+
+    //Memory Allocation
+    one = (struct node *)malloc(sizeof(struct node));
+    two = (struct node *)malloc(sizeof(struct node));
+    three = (struct node *)malloc(sizeof(struct node));
+
+    //Assigns values
+    one->c = 'A';
+    two->c = 'B';
+    three->c = 'C';
+
+    //Connects node 
+    one->link = two;
+    two->link = three;
+    three->link = NULL;
+
+    //printing node values
+    head = one;
+    print_list(head);
+    printf("\n");
+
 }
